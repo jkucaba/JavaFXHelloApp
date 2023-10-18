85a66e9745b4e398ddca36936b48e2110b766ed4
Jakub <jakub.kucaba@gmail.com>
2023-10-18 15:08:47 +0200

    Working on closing the app

--- src/main/java/com/jakubku/firstapp/HelloApplication.java
+++ src/main/java/com/jakubku/firstapp/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.jakubku.firstapp;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -18,12 +19,19 @@ public class HelloApplication extends Application {
         root.setSpacing(20); // wszytskie el jakie dodamy mają 20 przerwy
         Label text = new Label("");
         Button button = new Button("Click Me!");
+        Button button1 = new Button("Exit Explicitly"); //Dlugi proces aby zasymulowac dłużenie się programu
         button.setOnAction(e -> text.setText("Welcome to JavaFX Application"));
-        root.getChildren().addAll(text, button);
+        button1.setOnAction(evnt -> Platform.exit());
+        root.getChildren().addAll(text, button, button1);
         Scene scene = new Scene(root, 350, 300); //tworzymy scenę (okienko)
         stage.setScene(scene); //dodajemy scenę na scenę
         stage.setTitle("JavaFX Application");
         stage.show();
+    }
+    @Override
+    public void stop(){
+        System.out.println(Thread.currentThread().getName());
+        System.out.println("Closing the Application");
     }
 
     public static void main(String[] args) {
