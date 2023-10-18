8b3ee0d2c0167924ea2627cffc1ef8f79efe9353
Jakub <jakub.kucaba@gmail.com>
2023-10-18 14:13:20 +0200

    Same App Diff aproach

--- src/main/java/com/jakubku/firstapp/HelloApplication.java
+++ src/main/java/com/jakubku/firstapp/HelloApplication.java
@@ -1,19 +1,28 @@
 package com.jakubku.firstapp;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
-import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+    public void start(Stage stage){
+        VBox root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.setPadding(new Insets(20, 20, 20, 20));
+        root.setSpacing(20); // wszytskie el jakie dodamy mają 20 przerwy
+        Label text = new Label("");
+        Button button = new Button("Click Me!");
+        button.setOnAction(e -> text.setText("Welcome to JavaFX Application"));
+        root.getChildren().addAll(text, button);
+        Scene scene = new Scene(root, 350, 300); //tworzymy scenę (okienko)
+        stage.setScene(scene); //dodajemy scenę na scenę
+        stage.setTitle("JavaFX Application");
         stage.show();
     }
 
